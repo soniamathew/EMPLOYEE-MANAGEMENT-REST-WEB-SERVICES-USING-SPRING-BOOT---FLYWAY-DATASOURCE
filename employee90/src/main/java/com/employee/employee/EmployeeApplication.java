d660a02dfb47fe27c57c25aaffc2f57db3fa7291
soniamathew <40623767+soniamathew@users.noreply.github.com>
2018-09-05 14:58:38 +0530

    Update EmployeeApplication.java

--- employee90/src/main/java/com/employee/employee/EmployeeApplication.java
+++ employee90/src/main/java/com/employee/employee/EmployeeApplication.java
@@ -2,12 +2,8 @@ package com.employee.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import java.io.*;
-
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.employee.employee.*"})
 @EntityScan(basePackages = {"com.employee.employee.*"})
 
 public class EmployeeApplication {
